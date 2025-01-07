5c23b452de03bef458a4426b439fcac1ef2ab318
MisaHariRabe <misaharirabe58@gmail.com>
2025-01-07 15:48:59 +0300

    feat(user model):  get user by id

--- development/models/user_model.py
+++ development/models/user_model.py
@@ -3,7 +3,15 @@ from db import connection
 class UserModel:
     @staticmethod
     def create(user_name, user_dateofbirth, user_email, user_password):
-        cursor = connection.cursor()
-        sql = "INSERT INTO user (user_name, user_dateofbirth, user_email, user_password) VALUES (?,?,?,?);"
-        cursor.execute(sql, (user_name, user_dateofbirth, user_email, user_password))
-        connection.commit()
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO user (user_name, user_dateofbirth, user_email, user_password) VALUES (?,?,?,?);"
+            cursor.execute(sql, (user_name, user_dateofbirth, user_email, user_password))
+            connection.commit()
+
+    @staticmethod
+    def get_by_id(user_id):
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM user WHERE user_id = ?;"
+            cursor.execute(sql, (user_id,))
+            user = cursor.fetchone()
+            return user
