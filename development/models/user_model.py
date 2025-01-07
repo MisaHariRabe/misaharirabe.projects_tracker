7a0b61a5fee2f8d02005c59b189a5b66037fd03d
MisaHariRabe <misaharirabe58@gmail.com>
2025-01-07 15:51:36 +0300

    fix (user model): fix the sqlite3.Cursor context error

--- development/models/user_model.py
+++ development/models/user_model.py
@@ -3,14 +3,14 @@ from db import connection
 class UserModel:
     @staticmethod
     def create(user_name, user_dateofbirth, user_email, user_password):
-        with connection.cursor() as cursor:
+            cursor = connection.cursor()
             sql = "INSERT INTO user (user_name, user_dateofbirth, user_email, user_password) VALUES (?,?,?,?);"
             cursor.execute(sql, (user_name, user_dateofbirth, user_email, user_password))
             connection.commit()
 
     @staticmethod
     def get_by_id(user_id):
-        with connection.cursor() as cursor:
+            cursor = connection.cursor()
             sql = "SELECT * FROM user WHERE user_id = ?;"
             cursor.execute(sql, (user_id,))
             user = cursor.fetchone()
